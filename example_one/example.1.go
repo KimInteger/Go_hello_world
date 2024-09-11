2be871764f2bb9db184a028b61127b641860632a
김정수 <kjs20151240@gmail.com>
2024-09-11 22:22:56 +0900

    prac | example.1.go
    변수 선언하고 재할당하고 print해보기

---
+++ example_one/example.1.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a int = 10
+	var msg string = "hello Variable"
+
+	a = 20
+	msg = "Good Evening"
+
+	fmt.Println(msg, a)
+}
---
+++ example_one/go.mod
@@ -0,0 +1,3 @@
+module example_one
+
+go 1.23.1
