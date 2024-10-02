a6d6312d6e562a28e920e5a35f9d2426c7e40682
김정수 <kjs20151240@gmail.com>
2024-10-02 20:03:39 +0900

    test | search_primes.go
    기능테스트 완료

---
+++ primes/search_primes.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"primes/find"
+)
+
+func main() {
+	var start, end int
+
+	fmt.Print("소수를 찾고자 하는 시작 숫자를 입력하세요! 2이상! 2이하로 적으면 2가됩니다!")
+	fmt.Scan(&start)
+	fmt.Print("어디까지 찾을래요?")
+	fmt.Scan(&end)
+
+	primesList := find.FindPrimesInRange(start, end)
+
+	fmt.Printf("%d부터 %d까지의 소수는 %v\n입니다", start, end, primesList)
+}
